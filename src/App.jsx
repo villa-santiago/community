cc7842b42310c5ac4f0e4800cae37113cc1f49e2
Santiago Villa <code@santiagovilla.it>
2025-06-27 16:02:21 -0500

    started working on Auth Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-
-
 import './App.css'
+import PingTest from './components/PingTest';
+import HomePage from './pages/HomePage';
 
 function App() {
- 
-
   return (
-    <>
+    <div>
       <PingTest />
-    </>
-  )
+      <HomePage />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,7 @@
+function HomePage(){
+    return(
+        <div><h1>Hello</h1></div>
+    )
+}
+
+export default HomePage;
